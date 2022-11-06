e771d4c75c73ec20b10eba86b27318b84bd8c7eb
rama709 <82271128+rama709@users.noreply.github.com>
2022-11-06 21:00:19 +0530

    Create two missing numbers.cpp

---
+++ math/two missing numbers.cpp
@@ -0,0 +1,15 @@
+for 1 missing number:
+  sumof first N natural number(sum1)=n(n+1)/2;
+  sum of square of first n natural numbers(sigma(n2)--(sum2)=n(n+1)(2n+1)/6;
+                                           x+y=sum-sumof(givenarray);
+                                           x2+y2=(sum)2-(sumof (every element square)
+                                                         
+                                                         (x+y)2=x2+y2+2xy;
+                                                         2xy=(x+y)2-x2-y2
+                                                         xy=(x+y)2-(x2+y2)/2;
+                                                         now we know xy
+                                                         (x-y)2=x2+y2-2xy;
+                                                         we can get x-y
+                                                         x+y+x-y=x
+                                                         y=x-y
+                                                         therefore x and y are sloved.
