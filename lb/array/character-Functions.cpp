b5f6da201d176261f8e880d088e195b438d13b2b
rama709 <82271128+rama709@users.noreply.github.com>
2022-12-01 20:43:29 +0530

    Create character-Functions.cpp

---
+++ lb/array/character-Functions.cpp
@@ -0,0 +1,15 @@
+source:-https://www.softwaretestinghelp.com/cpp-character-functions/#tolower
+==================================================================================
+  No	Function name	         Function Purpose
+1	   isalnum	            Checks if a given character is alphanumeric
+2	   isalpha               	Checks if the character is alphabetic
+3	   isblank	                Checks if the character is blank
+4	  iscntrl	                Checks if the given character is a control character
+5	  isdigit	                Checks if the given character is a decimal digit
+6	  isgraph               	Checks if the given character has a graphical representation
+7	  islower	                Checks if a given character is lowercase
+8	  isprint                	Checks if the character is a printable character
+9	  ispunct              	Checks if the character is a punctuation character
+10	isspace             	Checks if the character is a white-space
+11	isupper	              Checks if the given character is uppercase
+12	isxdigit	            Checks if the character hexadecimal digit
